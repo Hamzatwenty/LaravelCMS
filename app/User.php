400c61dcab173e2fb1e8e3acaa008016d74296fd
Hamzatwenty <hamza.waheed00777@gmail.com>
2020-05-19 15:41:48 +0500

    users adding with photos

--- app/User.php
+++ app/User.php
@@ -27,4 +27,8 @@ class User extends Authenticatable
     public function role(){
         return $this->belongsTo(Role::class);
     }
+
+    public function photo(){
+        return $this->belongsTo(Photo::class);
+    }
 }
